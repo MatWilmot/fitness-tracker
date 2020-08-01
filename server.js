2c6fbb6b90ac740ebf77b380cfaf3d53cfc7966c
Mat Wilmot <matwilmot90@gmail.com>
2020-08-01 08:35:21 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/shoes_db", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+// require routes
+const publicRoutes = require("./routes/public-routes.js");
+// use routes
+app.use(publicRoutes);
+
+app.listen(PORT, () => console.log(`Listening at: http://localhost:${PORT}`));
